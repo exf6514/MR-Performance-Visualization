235386e518e993af1d293e232a489cff8d2e2e5b
exf6514 <exf6514@cs.rit.edu>
2019-02-12 15:30:34 -0500

    added Nikolas work. Added ability to select file. Data for Gcpu now displays

---
+++ MR-Performance-Visualization/MR-Performance-Visualization/GlobalProcess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MR_Performance_Visualization
+{
+    class GlobalProcess
+    {
+        public string Timestamp { get; set; }
+        public int Pid { get; set; }
+        public int Tid { get; set; }
+        public double Gcpu { get; set; }
+        public double? GcpuPeak { get; set; }
+        public int Ghc { get; set; }
+        public int? GhcPeak { get; set; }
+
+        public GlobalProcess(string timestamp, int pid, int tid, double gcpu, double? gcpuPeak, int ghc, int? ghcPeak)
+        {
+            Timestamp = timestamp;
+            Pid = pid;
+            Tid = tid;
+            Gcpu = gcpu;
+            GcpuPeak = gcpuPeak;
+            Ghc = ghc;
+            GhcPeak = ghcPeak;
+        }
+    }
+}
