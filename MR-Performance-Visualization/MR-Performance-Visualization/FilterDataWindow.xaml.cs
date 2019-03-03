4abd5488da2da30d54cb9eaadd11f210370172ce
exf6514 <exf6514@cs.rit.edu>
2019-03-03 17:13:48 -0500

    can now load result file

--- MR-Performance-Visualization/MR-Performance-Visualization/FilterDataWindow.xaml.cs
+++ MR-Performance-Visualization/MR-Performance-Visualization/FilterDataWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
@@ -31,6 +32,19 @@ namespace MR_Performance_Visualization
 
             List<string> metrics = new List<string>(new string[] { "CPU", "HC", "PRIV" });
             List<string> comparators = new List<string>(new string[] { ">", ">=", "=", "<=", "<" });
+            // set up combo boxes
+            foreach (string m in metrics)
+            {
+                metric_cb.Items.Add(m);
+            }
+            metric_cb.SelectedIndex = 0;
+
+            foreach (string c in comparators)
+            {
+                comparator_cb.Items.Add(c);
+            }
+            comparator_cb.SelectedIndex = 0;
+
 
             tfps = TraceFileParserSingleton.Instance;
 
@@ -44,17 +58,7 @@ namespace MR_Performance_Visualization
                 }
                 process_name_cb.SelectedIndex = 0;
 
-                foreach (string m in metrics)
-                {
-                    metric_cb.Items.Add(m);
-                }
-                metric_cb.SelectedIndex = 0;
-
-                foreach (string c in comparators)
-                {
-                    comparator_cb.Items.Add(c);
-                }
-                comparator_cb.SelectedIndex = 0;
+                Associated_Trace_File_label.Content = "UTR File: " + tfps.Filename;
             }
 
         }
@@ -167,7 +171,7 @@ namespace MR_Performance_Visualization
                 this.Cursor = Cursors.Arrow; //back to useable cursor
                 filterProcesses = filteredValues;
                 process_dg.ItemsSource = filteredValues; //set data for table
-
+                Associated_Trace_File_label.Content = "UTR File: " + tfps.Filename;
                 //store last searched query
                 LastSearchString = processName + "|" + metricName + "|" + comparator + "|" + searchValue;
             }
@@ -176,12 +180,62 @@ namespace MR_Performance_Visualization
         private void Load_Button_Click(object sender, RoutedEventArgs e)
         {
             //read in data
-            
-            //parse data
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Result Files|*.txt";
+            ofd.Title = "Select a Result File";
+            ofd.RestoreDirectory = true;
 
-            //populate the search elements
+            if (ofd.ShowDialog() == true)
+            {
+                string filepath = ofd.FileName;
+                string[] lines = File.ReadAllLines(filepath);
 
-            //populate the table
+                //populate the search items
+                Associated_Trace_File_label.Content = "UTR File: " + lines[0];
+
+                string[] queryParts = lines[1].Split('|'); // name, metric, comparator, value
+
+                var nameIndex = process_name_cb.Items.IndexOf(queryParts[0]);
+                if (nameIndex >= 0)
+                {
+                    process_name_cb.SelectedIndex = nameIndex;
+                }
+                else
+                {
+                    int newIndex = process_name_cb.Items.Add(queryParts[0]);
+                    process_name_cb.SelectedIndex = newIndex;
+                }
+
+                metric_cb.SelectedIndex = metric_cb.Items.IndexOf(queryParts[1]);
+                comparator_cb.SelectedIndex = comparator_cb.Items.IndexOf(queryParts[2]);
+                value_tb.Text = queryParts[3];
+
+                //parse process data
+
+                lines = lines.Where((item, index) => index > 1).ToArray(); //gets rid of first two lines, leaving process data;
+                if (lines.Count() > 0)
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] data = line.Split('|'); //timestamp, name, cpu, hc, priv
+                        Process p = new Process(
+                            data[0],//timestamp
+                            data[1],//name
+                            double.Parse(data[3]),//hc
+                            null,
+                            double.Parse(data[4]),//priv
+                            null,
+                            double.Parse(data[2]),//cpu
+                            null
+                            );
+                        filterProcesses.Add(p);
+                    }
+
+                    process_dg.ItemsSource = filterProcesses;
+
+                }
+
+            }
         }
 
         private void Save_Button_Click(object sender, RoutedEventArgs e)
