4edf14b5882546da600914adf940d650b77219f4
Justin Namba <jrn1325@rit.edu>
2019-04-14 11:26:39 -0400

    Update MenuController.cs
    Added comments

--- MR-Performance-Visualization/MR-Performance-Visualization/MenuController.cs
+++ MR-Performance-Visualization/MR-Performance-Visualization/MenuController.cs
@@ -6,17 +6,28 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
+// Creates a namespace
 namespace MR_Performance_Visualization
 {
+    /**
+    * Creates a class called MenuController that manages buttons functions
+    */
     class MenuController
     {
+        // Instantiate Utils constructor
         Utils utils = new Utils();
 
+        /**
+        * Shutdowns the application
+        */
         public static void Power_Button_Click()
         {
             Application.Current.Shutdown();
         }
 
+        /**
+        * Shows application settings
+        */
         public void Settings_Button_Click(Window window)
         {
             SettingsWindow settingsWindow = new SettingsWindow();
@@ -25,6 +36,9 @@ namespace MR_Performance_Visualization
             utils.ClearEffect(window);
         }
 
+        /**
+        * Provides helpful information to users
+        */
         public void Help_Button_Click(Window window)
         {
             HelpWindow helpWindow = new HelpWindow();
@@ -33,6 +47,9 @@ namespace MR_Performance_Visualization
             utils.ClearEffect(window);
         }
 
+        /**
+        * Closes the application
+        */
         public static void Close_Button_Click(Window window)
         {
             window.Close();
